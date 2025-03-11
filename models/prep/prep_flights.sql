86496c6dc2a575fd2342851f78cb4eb5b412e1e4
Pooneh Nezam <pooneh_nezam@Poonehs-MacBook-Air.local>
2025-03-11 20:13:58 +0100

    meme

--- models/prep/prep_flights.sql
+++ models/prep/prep_flights.sql
@@ -24,6 +24,6 @@ SELECT flight_date::DATE
         ,(distance * 0.621371)::NUMERIC(6,2) AS distance_km
         ,cancelled
         ,diverted
-FROM flights_january
+FROM flights_one_month
 )
 SELECT * FROM flights_cleaned
